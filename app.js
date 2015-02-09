e3b8a5d30c97e7a7e38e22026b52bafac7245b7a
Luke Albao <lukealbao@gmail.com>
2015-02-09 17:19:37 -0500

    fixed winning move bug

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ io.sockets.on('connection', function (socket) {
 	dealer.processMoveRequest(request, function (err, res) {
 	    var update = {};
 	    socket.emit('Move Response', err || res);
-	    if (!err && res.game.active) {
+	    if (!err) {
 		io.to(app.connectedUsers[res.nextMove])
 		    .emit('Your Move', res);
 	    }
